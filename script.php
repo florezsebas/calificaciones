fcddab2533d327db118c1f2801a86478a612326b
Sebastian Florez <florezsebas@utp.edu.co>
2018-05-18 13:50:34 +0000

    Se agrega la linea ->save(); y se cambian los campos codigo_acudiente y codigo

--- script.php
+++ script.php
@@ -1,29 +1,30 @@
-$jor = new Jornada;
+$jor = new App\Jornada;
 $jor->nombre = "Jornada manana";
 $jor->hora_inicio = "2018:05:17 07:30:00";
 $jor->hora_fin = "2018:05:17 07:30:00";
 $jor->save();
 
-$gra = new Grado;
+$gra = new App\Grado;
 $gra->nombre = "Septimo";
 $gra->save();
 
-$grp = new Grupo;
+$grp = new App\Grupo;
 $grp->nombre = "A";
 $grp->jornada_id = 1;
 $grp->grado_id = 1;
+$grp->save();
 
-$acu = new Acudiente;
-$acu->codigo = "1";
+$acu = new App\Acudiente;
+$acu->codigo = "000";
 $acu->nombres = "Carlos Alberto";
 $acu->apellidos = "Acosta Castano";
 $acu->email = "caralb@gmail.com";
 $acu->password = bcrypt('salado');
 $acu->save();
 
-$est = new Estudiante;
-$est->codigo = "1";
-$est->codigo_acudiente = "1";
+$est = new App\Estudiante;
+$est->codigo = "111";
+$est->codigo_acudiente = "000";
 $est->grupo_id = 1;
 $est->nombres = "Carlos Mario";
 $est->apellidos = "Otalvaro Garcia";
