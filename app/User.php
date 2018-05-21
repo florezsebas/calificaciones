cc4d1a9969355f2f8cf9dcacf87bf2c94cc239b1
cjgc <chjugomez@utp.edu.co>
2018-05-21 19:47:44 +0000

    Se agrego el campo apellidos

--- app/User.php
+++ app/User.php
@@ -15,7 +15,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nombres', 
+        'apellidos', 
+        'email', 
+        'password',
     ];
 
     /**
