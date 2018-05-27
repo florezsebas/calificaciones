a5cf794ada1312251b0e1f6ada8bfb602bd7512f
cjgc <chjugomez@utp.edu.co>
2018-05-27 15:16:18 +0000

    Se modifica para hacer uso del paquete para la creacion de formularios

--- config/app.php
+++ config/app.php
@@ -159,7 +159,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        /*
+         * Providers Personales
+         */
+        Collective\Bus\BusServiceProvider::class,
     ],
 
     /*
@@ -208,7 +212,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        
     ],
 
 ];
