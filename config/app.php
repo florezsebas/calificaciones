11972ed97e57185b4a5331203c80682ad67f31a0
cjgc <chjugomez@utp.edu.co>
2018-05-27 21:06:56 +0000

    Se agrego la configuracion necesaria para el paquete Laravel Collective para la realizacion de formularios

--- config/app.php
+++ config/app.php
@@ -213,6 +213,13 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         
+        /*
+         * Aliases Personales
+         */
+         
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        
     ],
 
 ];
