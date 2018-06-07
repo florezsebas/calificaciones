b648ec8fa90b1b4abb378541b2a42dc2bad456c5
cjgc <chjugomez@utp.edu.co>
2018-06-07 19:47:38 +0000

    Se ajusta la vista creacion de periodos

--- resources/views/admin/periodos/create.blade.php
+++ resources/views/admin/periodos/create.blade.php
@@ -1,20 +1,21 @@
+@extends('layouts.admin')
+
 @section('content')
 <h2>Crear nuevo periodo</h2> <br>
-
-{!! Form::open() !!}
+{!! Form::open(['route' => 'periodos.store']) !!}
     <div class="form-group">
         {!! Form::label('nombre', 'Nombre') !!}
         {!! Form::text('nombre', null, ['class' => 'form-control', 'placeholder' => 'Nombre del periodo', 'required']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('hora_inicio', 'Fecha de inicio') !!}
-        {!! Form::date('hora_inicio', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
+        {!! Form::label('fecha_inicio', 'Fecha de inicio') !!}
+        {!! Form::date('fecha_inicio', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('hora_fin', 'fecha de finalizacion')!!}
-        {!! Form::date('hora_fin', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
+        {!! Form::label('fecha_fin', 'fecha de finalizacion')!!}
+        {!! Form::date('fecha_fin', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
     </div>
     {!! Form::submit('Agregar periodo', ['class' => 'btn btn-primary']) !!}
-    <a href="" class="btn btn-primary">Cancelar</a>
+    <a href="{{ route('periodos.index') }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
 @endsection
