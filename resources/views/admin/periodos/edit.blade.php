ce97c107ade455ff024a95b34b5e33cf6520b7e7
cjgc <chjugomez@utp.edu.co>
2018-06-07 19:48:10 +0000

    Se ajusta la vista de edicion de periodos

--- resources/views/admin/periodos/edit.blade.php
+++ resources/views/admin/periodos/edit.blade.php
@@ -1,20 +1,21 @@
-@section('content')
+@extends('layouts.admin')
 
+@section('content')
 <h2>Editar periodo</h2> <br>
-{!! Form::open() !!}
+{!! Form::open(['route' => ['periodos.update', $periodo], 'method' => 'put']) !!}
     <div class="form-group">
         {!! Form::label('nombre', 'Nombre') !!}
-        {!! Form::text('nombre', null, ['class' => 'form-control', 'placeholder' => 'Nombre del periodo', 'required']) !!}
+        {!! Form::text('nombre', $periodo->nombre, ['class' => 'form-control', 'placeholder' => 'Nombre del periodo', 'required']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('hora_inicio', 'Hora de inicio') !!}
-        {!! Form::date('hora_inicio', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
+        {!! Form::label('fecha_inicio', 'Hora de inicio') !!}
+        {!! Form::date('fecha_inicio', $periodo->fecha_inicio, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('hora_fin', 'Hora de finalizacion')!!}
-        {!! Form::date('hora_fin', null, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
+        {!! Form::label('fecha_fin', 'Hora de finalizacion')!!}
+        {!! Form::date('fecha_fin', $periodo->fecha_fin, ['class' => 'form-control', 'placeholder' => 'HH:MM:SS', 'required']) !!}
     </div>
     {!! Form::submit('Agregar periodo', ['class' => 'btn btn-primary']) !!}
-    <a href="" class="btn btn-primary">Cancelar</a>
+    <a href="{{ route('periodos.index') }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
 @endsection
