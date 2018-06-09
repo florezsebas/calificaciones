4cdfd015785b223aa7b9c333bf26cf9ad631ef3b
cjgc <chjugomez@utp.edu.co>
2018-06-09 00:40:00 +0000

    Se ajusta la visualizacion de datos en la tabla a partir de cursos

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-<h2>Gestion de grupos</h2> <br>
-<a href="" class="btn btn-primary" style="margin-bottom:1em">Nuevo grupo</a>
+<h2>Gestion de cursos</h2> <br>
+<a href="{{ route('cursos.create') }}" class="btn btn-primary" style="margin-bottom:1em">Nuevo curso</a>
 
 <!-- Tabla que muestra las jornadas -->
 <table class="table table-sm">
@@ -11,22 +11,23 @@
       <th scope="col">#</th>
       <th scope="col">Nombre</th>
       <th scope="col">Profesor</th>
-      <th scope="col">Grupo-Grado</th>
+      <th scope="col">Grupo</th>
       <th scope="col">Accion</th>
     </tr>
   </thead>
   <tbody>
+    @foreach($cursos as $curso)
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{{ $curso->id }}</td>
+        <td>{{ $curso->docente->user->nombres }}</td>
+        <td>{{ $curso->docente->user->apellidos . $cursos->docente->apellidos}}</td>
+        <td>{{ $curso->grupo->nombre }}</td>
         <td>
-            <a href="" class="btn btn-primary btn-xs pull-left">Editar</a>
-            <a href="" class="btn btn-danger btn-xs pull-left">Eliminar</a></td>
+            <a href="{{ route('cursos.edit','$curso->id') }}" class="btn btn-primary btn-xs pull-left">Editar</a>
+            <a href="{{ route('cusos.destroy', $curso->id) }}" class="btn btn-danger btn-xs pull-left">Eliminar</a>
         </td>
-        
       </tr>
+    @endforeach
   </tbody>
 </table>
 @endsection
