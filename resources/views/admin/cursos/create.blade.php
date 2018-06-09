a5caf8ef1639c45ca00f33fc480c3a9a2ba7cee3
cjgc <chjugomez@utp.edu.co>
2018-06-09 00:38:00 +0000

    Se ajusta la vista para adicianar el despliegue de grupos apartir de la seleccion de un grado

--- resources/views/admin/cursos/create.blade.php
+++ resources/views/admin/cursos/create.blade.php
@@ -2,21 +2,26 @@
 
 @section('content')
 <h2>Crear nuevo curso</h2> <br>
-{!! Form::open() !!}
+{!! Form::open(['route' => 'cursos.store']) !!}
     <div class="form-group">
         {!! Form::label('nombre', 'Nombre') !!}
         {!! Form::text('nombre', null, ['class' => 'form-control', 'placeholder' => 'Nombre del curso', 'required']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('profesor_id', 'Docente') !!}
-        {!! Form::select('profesor_id', $docente=['10' => 'Edgar Trujillo'], $selected=null, ['class' => 'form-control','placeholder' => 'Profesor a cargo', 'required']) !!}
+        {!! Form::select('profesor_id', $docentes, $selected=null, ['class' => 'form-control','placeholder' => 'Profesor a cargo', 'required']) !!}
+    </div>
+    
+    <div class="form-group">
+        {!! Form::label('grado_id', 'Grado') !!}
+        {!! Form::select('grado_id', $grados, $selected=null, ['class' => 'form-control','placeholder' => 'Grado', 'required', 'id' => 'grado']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('grupo_id', 'Grupo') !!}
-        {!! Form::select('grupo_id', $grupos=['10' => 'Septimo A'], $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione el grupo', 'required']) !!}
+        {!! Form::select('grupo_id', [], $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione el grupo', 'required', 'id' => 'grupo']) !!}
     </div>
-    {!! Form::submit('Agregar grupo', ['class' => 'btn btn-primary']) !!}
-    <a href="" class="btn btn-primary">Cancelar</a>
+    {!! Form::submit('Agregar curso', ['class' => 'btn btn-primary']) !!}
+    <a href=" {{ route('cursos.index') }} " class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
-
+{{ Html::script('js/cursos/desplegarGrupos.js'),array(),true }}
 @endsection
