62b2a3be809dd43ab6f148ce91e81eb7c5c60099
cjgc <chjugomez@utp.edu.co>
2018-06-14 16:52:04 +0000

    Controlador base para calificaciones

---
+++ app/Http/Controllers/Docente/CalificacionesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Docente;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Jornada;
+
+class CalificacionesController extends Controller
+{
+    public function index()
+    {
+        $jornadas = Jornada::all()->pluck('nombre','id');
+        return view('docentes.calificaciones.index')->with('jornadas',$jornadas);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
