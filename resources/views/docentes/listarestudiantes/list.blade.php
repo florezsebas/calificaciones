39cf59d256edfd74b300fbd4e4be88f6be72c950
florezsebas <florezsebas@utp.edu.co>
2018-06-14 22:11:27 +0000

    Se agrega la vista para mostrar los estudiantes de un grado, grupo, y curso seleccionado

---
+++ resources/views/docentes/listarestudiantes/list.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.docentes')
+
+@section('content')
+<h4>Estudiantes del curso de {{$curso->nombre}} del grado {{ $grupo->grado->nombre }} grupo {{$grupo->nombre}} </h4>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Codigo</th>
+                <th scope="col">Correo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($estudiantes as $estudiante)
+                <tr>
+                    <td>{{ $estudiante->user_id }}</td>
+                    <td>{{ $estudiante->user->apellidos }}</td>
+                    <td>{{ $estudiante->user->nombres }}</td>
+                    <td>{{ $estudiante->user->codigo }}</td>
+                    <td>{{ $estudiante->user->email }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
