3326280a40c60555a5b6f892b0206ef1feb2bd61
cjgc <chjugomez@utp.edu.co>
2018-06-14 19:52:16 +0000

    Controlador actulizado y terminado

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -3,20 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laracasts\Flash\Flash;
 use App\Curso;
 use App\Grupo;
 use App\Grado;
 use App\User;
 use App\Docente;
-use DB;
+use App\Jornada;
 
 class CursosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function loadGrados(Request $request) {
+        if($request->ajax()){
+            $grados = Grado::where("jornada_id",$request->jornada_id)->pluck('nombre','id');
+            return response()->json($grados);
+        }
+    }
+    
+    public function loadGroups(Request $request) {
+        if($request->ajax()){
+            $grupos = Grupo::where("grado_id",$request->grado_id)->pluck('nombre','id');
+            return response()->json($grupos);
+        }
+     }
+
     public function index()
     {
         $cursos = Curso::orderBy('id', 'ASC')->paginate(5);
@@ -26,99 +36,59 @@ class CursosController extends Controller
         });
         return view('admin.cursos.index')->with('cursos',$cursos);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-     
-    public function getGroup(Request $request) {
-        if($request->ajax()){
-            // $grados = Grupo::all()->pluck('nombre','id');
-            $grupos = Grupo::where("grado_id",$request->grado_id)->pluck('nombre','id');
-            return response()->json($grupos);
-        }
-     }
      
     public function create(Request $request)
     {
-        
-        $grados = Grado::all()->pluck('nombre','id');
-        $grupos = Grupo::all()->pluck('nombre','id');
-        
-        /* ---------------- preparacion de docente-usuario ---------------- */
-        $users = User::has('docente')->get();
-        // dd($users);
-        $docentes = array();
-        foreach($users as $user){
-            $codigo = $user->codigo;
-            $nombre_completo = $user->docente->nombres . " " . $user->docente->apellidos;
-            array_push($docentes, $codigo);
-        }
-        //dd($docentes);
-        return view('admin.cursos.create')->with('grados',$grados)
-                                          ->with('grupos', $grupos)
-                                          ->with('docentes', $docentes);
+        $jornadas = Jornada::all()->pluck('nombre','id');
+        $docentes = User::has('docente')->get()->pluck('full_name','id');
+        return view('admin.cursos.create')->with('docentes', $docentes)
+                                          ->with('jornadas',$jornadas);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $curso = new Curso($request->all());
-        //$curso->save();
-        //Flash::success("Se ha registrado el curso " . $curso->nombre . " de forma exitosa!");
-        dd($request->all());
+        $curso->save();
+        Flash::success("Se ha registrado el curso ".$curso->nombre." exitosamente!");
         return redirect()->route('cursos.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $curso = Curso::find($id);
+        $docentes = User::has('docente')->get()->pluck('full_name','id');
+        $jornadas = Jornada::all()->pluck('nombre','id');
+        $grupos = Grupo::all()->pluck('nombre', 'id');
+        $grados = Grado::all()->pluck('nombre', 'id');
+        
+        return view('admin.cursos.edit')->with('curso',$curso)
+                                        ->with('grupos',$grupos)
+                                        ->with('grados', $grados)
+                                        ->with('docentes',$docentes)
+                                        ->with('jornadas',$jornadas);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $curso = Curso::find($id);
+        $curso->nombre = $request->nombre;
+        $curso->docente_id = $request->docente_id;
+        $curso->grupo_id = $request->grupo_id;
+        $curso->save();
+        Flash::success("Se ha editado el curso ".$curso->nombre." exitosamente!");
+        return redirect()->route('cursos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $curso = Curso::find($id);
+        $curso->delete();
+        Flash::warning("El curso ".$curso->nombre." se ha eliminado correctamente");
+        return redirect()->route('cursos.index');
     }
 }
