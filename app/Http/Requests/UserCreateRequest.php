e1617d8ee25c209de764136ba5e532997fb886eb
cjgc <chjugomez@utp.edu.co>
2018-06-14 20:08:13 +0000

    Espaceado de los elementos de la lista

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -4,31 +4,20 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class UserCreateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            'codigo'     => 'min:6|max:50|required|unique:users',
-            'nombres'    => 'min:3|max:120|required|string',
-            'apellidos'  => 'min:3|max:120|required|string',
-            'email'      => 'required|string|email|max:255|unique:users',
-            
+            'codigo'           =>     'min:6|max:50|required|unique:users',
+            'nombres'          =>     'min:3|max:120|required|string',
+            'apellidos'        =>     'min:3|max:120|required|string',
+            'email'            =>     'required|string|email|max:255|unique:users',
         ];
     }
 }
