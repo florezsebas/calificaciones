1bc3a282521ab6562eaf6ca44682e6cd28805685
cjgc <chjugomez@utp.edu.co>
2018-06-14 20:11:17 +0000

    Se modifica el seeder de acuerdo a los cambios en las tablas users, docentes, estudiantes y acudientes

--- database/seeds/DocentesTableSeeder.php
+++ database/seeds/DocentesTableSeeder.php
@@ -13,45 +13,45 @@ class DocentesTableSeeder extends Seeder
     {
         
         DB::table('users')->insert([
+            'codigo' => '108800055',
             'nombres' => 'Carlos Alberto',
             'apellidos' => 'Gonzales otalvaro',
-            'fecha_nacimiento' => '3000-11-30',
+            'fecha_nacimiento' => '1975-11-30',
             'email' => 'cargon@example.com',
             'tipo' => 'docente',
-            'password' => bcrypt('contrasena'),
+            'password' => bcrypt('123456'),
         ]);
         
 
         DB::table('users')->insert([
-            'nombres' => 'Mario ramirez',
-            'apellidos' => 'otano caicedo',
+            'codigo' => '96656540',
+            'nombres' => 'Mario',
+            'apellidos' => 'Caicedo Mazo',
             'fecha_nacimiento' => '2005-07-09',
             'email' => 'margo@example.com',
             'tipo' => 'docente',
-            'password' => bcrypt('contrasena'),
+            'password' => bcrypt('123456'),
         ]);
 
         DB::table('users')->insert([
-            'nombres' => 'Sebastian florez',
-            'apellidos' => 'guerrero',
-            'fecha_nacimiento' => '0001-01-01',
+            'codigo' => '54884140',
+            'nombres' => 'Sebastian',
+            'apellidos' => 'Florez Guerrero',
+            'fecha_nacimiento' => '1994-01-01',
             'email' => 'sebasflorez@example.com',
             'tipo' => 'docente',
-            'password' => bcrypt('contrasena'),
+            'password' => bcrypt('123456'),
         ]);
         
         DB::table('docentes')->insert([
-            'codigo' => '0000000000',
             'user_id' => DB::table('users')->where('nombres','Carlos Alberto')->value('id'),
         ]);
         
         DB::table('docentes')->insert([
-            'codigo' => '0000000001',
             'user_id' => DB::table('users')->where('nombres','Mario ramirez')->value('id'),
         ]);
         
         DB::table('docentes')->insert([
-            'codigo' => '0000000002',
             'user_id' => DB::table('users')->where('nombres','Sebastian florez')->value('id'),
         ]);
     }
