5ec64bba9326651f832c726e729ef2fc11051637
florezsebas <florezsebas@utp.edu.co>
2018-06-17 01:29:39 +0000

    Se ajusta el edit para editar observaciones

--- resources/views/docentes/observaciones/edit.blade.php
+++ resources/views/docentes/observaciones/edit.blade.php
@@ -1,26 +1,25 @@
-@extends('layouts.admin')
+@extends('layouts.docentes')
 
 @section('content')
-<h2>Editar curso</h2> <br>
-{!! Form::open(['route' => ['cursos.update', $curso], 'method' => 'put']) !!}
-    <div class="form-group">
-        {!! Form::label('nombre', 'Nombre') !!}
-        {!! Form::text('nombre', $curso->nombre, ['class' => 'form-control', 'placeholder' => 'Nombre del curso', 'required']) !!}
+<h2>Editar observacion</h2> <br>
+{!! Form::open(['route' => ['observaciones.actualizar', $observacion], 'method' => 'put']) !!}
+  <div class="form-row">
+    <div class="form-group col-md-3">
+      {!! Form::label('titulo', 'Titulo') !!}
+      {!! Form::text('titulo', $observacion->titulo, ['class' => 'form-control','placeholder' => 'Titulo', 'required']) !!}
     </div>
-    <div class="form-group">
-        {!! Form::label('docente_id', 'Docente') !!}
-        {!! Form::select('docente_id', $docentes, $curso->docente_id, ['class' => 'form-control','placeholder' => 'Profesor a cargo', 'required']) !!}
+    <div class="form-group col-md-3">
+      {!! Form::label('periodo', 'Periodo') !!}
+      {!! Form::select('periodo', $periodos, $observacion->periodo_id, ['class' => 'form-control','placeholder' => 'Seleccione periodo', 'required']) !!}
     </div>
-    <div class="form-group">
-        {!! Form::label('grado_id', 'Grado') !!}
-        {!! Form::select('grado_id', $grados, $curso->grupo->grado_id, ['class' => 'form-control','placeholder' => 'Grado', 'required', 'id' => 'grado']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('grupo_id', 'Grupo') !!}
-        {!! Form::select('grupo_id', [], $curso->grupo->id, ['class' => 'form-control','placeholder' => 'Seleccione el grupo', 'required', 'id' => 'grupo']) !!}
-    </div>
-    {!! Form::submit('Editar curso', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('cursos.index') }}" class="btn btn-primary">Cancelar</a>
+  </div>
+  <div>
+    {!! Form::label('descripcion', 'Descripcion') !!}
+  </div>
+  <div class="form-group">
+    {!! Form::textarea('descripcion', $observacion->descripcion, ['class' => 'form-control','placeholder' => 'Descripcion', 'required']) !!}
+  </div>
+  {!! Form::submit('Editar observacion', ['class' => 'btn btn-primary']) !!}
+  <a href="{{ route('observaciones.listar', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
-{{ Html::script('js/cursos/desplegarGrupos.js'),array(),true }}
 @endsection
