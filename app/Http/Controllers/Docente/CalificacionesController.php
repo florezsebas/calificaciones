815fdff37cf4e271c3cb5e59f30abb0f7bc8021c
florezsebas <florezsebas@utp.edu.co>
2018-06-17 14:16:59 +0000

    Se ajusta metodo store

--- app/Http/Controllers/Docente/CalificacionesController.php
+++ app/Http/Controllers/Docente/CalificacionesController.php
@@ -4,43 +4,71 @@ namespace App\Http\Controllers\Docente;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Jornada;
+use App\Calificacion;
+use App\Curso;
+use App\Grupo;
+use App\Periodo;
+use App\Actividad;
+use Laracasts\Flash\Flash;
 
 class CalificacionesController extends Controller
 {
     public function index()
     {
-        $jornadas = Jornada::all()->pluck('nombre','id');
-        return view('docentes.calificaciones.index')->with('jornadas',$jornadas);
+        $cursos = Curso::all();
+        return view('docentes.calificaciones.index')->with('cursos',$cursos);
     }
 
-    public function create()
-    {
-        //
+    public function listingActivities(Request $request,$curso_id) {
+        $curso = Curso::find($curso_id);
+        $actividades = $curso->actividades;
+        return view('docentes.calificaciones.list')->with('curso',$curso)
+                                                   ->with('actividades',$actividades);
     }
 
-    public function store(Request $request)
-    {
-        //
+    public function listingStudents(Request $request, $curso_id, $act_id) {
+        $curso = Curso::find($curso_id);
+        $actividad = Actividad::find($act_id);
+        $estudiantes = $curso->grupo->estudiantes;
+        $periodos = Periodo::all()->pluck('nombre','id');
+        $calificaciones = Calificacion::where('actividad_id',$act_id)->get();
+        return view('docentes.calificaciones.listarestudiantes')->with('curso',$curso)
+                                                                ->with('estudiantes',$estudiantes)
+                                                                ->with('periodos',$periodos)
+                                                                ->with('actividad',$actividad)
+                                                                ->with('calificaciones',$calificaciones);
     }
 
-    public function show($id)
+    public function store(Request $request, $curso_id, $act_id)
     {
-        //
+        $calificaciones = Calificacion::where('actividad_id',$act_id)->get();
+        if(count($calificaciones) == 0) {
+            /* Seccion para crear la calificacion */
+            for($i=0; $i < count($request->calificaciones); $i++) {
+                $calificacion = new Calificacion();
+                $calificacion->valor = $request->calificaciones[$i];
+                $calificacion->estudiante_id = $request->codigos[$i];
+                $calificacion->curso_id = $curso_id;
+                $calificacion->periodo_id = $request->periodo;
+                $calificacion->actividad_id = $act_id;
+                $calificacion->save();
+            }
+        }
+        else { 
+            /* Seccion de actualizacion de calificacion */
+            $i=0;
+            foreach($calificaciones as $calificacion) {
+                $calificacion->valor = $request->calificaciones[$i];
+                //$calificacion->estudiante_id = $request->codigos[$i];
+                //$calificacion->curso_id = $curso_id;
+                $calificacion->periodo_id = $request->periodo;
+                //$calificacion->actividad_id = $act_id;
+                $calificacion->save();
+                $i++;
+            }
+        }
+        Flash::success("Se ha registrado las calificaciones exitosamente!");
+        return redirect()->route('calificaciones.actividades.list',$curso_id);
     }
 
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
-    }
 }
