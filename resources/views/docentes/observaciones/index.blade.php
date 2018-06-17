090d5e6a22e580397233fc10d7426ca1bc57e72d
florezsebas <florezsebas@utp.edu.co>
2018-06-17 01:28:47 +0000

    Se ajusta el index para gestionar observaciones

--- resources/views/docentes/observaciones/index.blade.php
+++ resources/views/docentes/observaciones/index.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Grupos</h2> <br>
-<!--<a href="{{ route('cursos.create') }}" class="btn btn-primary" style="margin-bottom:1em">Nuevo curso</a>-->
-{!! Form::open(['route' => 'cursos.store']) !!}
-  <div class="form-row">
-    <div class="form-group col-md-3">
-      {!! Form::label('jornada_id', 'Jornada') !!}
-      {!! Form::select('jornada_id', $jornadas, $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione jornada', 'required', 'id' => 'jornada']) !!}
-    </div>
-    <div class="form-group col-md-3">
-      {!! Form::label('grado_id', 'Grado') !!}
-      {!! Form::select('grado_id', [], $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione grado', 'required', 'id' => 'grado']) !!}
-    </div>
-    <div class="form-group col-md-3">
-      {!! Form::label('grupo_id', 'Grupo') !!}
-      {!! Form::select('grupo_id', [], $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione grupo', 'required', 'id' => 'grupo']) !!}
-    </div>
-    <div class="form-group col-md-3">
-      {!! Form::label('curso_id', 'Cursos') !!}
-      {!! Form::select('curso_id', [], $selected=null, ['class' => 'form-control','placeholder' => 'Seleccione curso', 'required', 'id' => 'curso']) !!}
-    </div>
-  </div>
-{!! Form::submit('Aceptar', ['class' => 'btn btn-primary']) !!}
-<a href="{{ url('docentes/') }}" class="btn btn-primary ">Cancelar</a>
-{!! Form::close() !!}
-{{ Html::script('js/docente/recursos.js'),array(),true }}
+<h2>Observaciones</h2> <br>
+
+<!-- Tabla que muestra los cursos -->
+<div class="table-responsive">
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Jornada</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Docente</th>
+        <th scope="col">Grado:Grupo</th>
+        <th scope="col">Accion</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($cursos as $curso)
+        <tr>
+          <td>{{ $curso->id }}</td>
+          <td>{{ $curso->grupo->grado->jornada->nombre }}</td>
+          <td>{{ $curso->nombre }}</td>
+          <td>{{ $curso->docente->user->nombres }}</td>
+          <td>{{ $curso->grupo->grado->nombre . " : "  . $curso->grupo->nombre }}</td>
+          <td>
+              <a href="{{ route('observaciones.estudiantes', $curso->id) }}" class="btn btn-primary btn-sm pull-left">Ver estudiantes</a>
+          </td>
+        </tr>
+      @endforeach
+    </tbody>
+</table>
+</div>
 @endsection
