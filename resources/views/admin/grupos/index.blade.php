eda7491bdea49ab28c00ac2a77a8243408b1a29e
florezsebas <florezsebas@utp.edu.co>
2018-06-18 19:51:14 +0000

    Se ajusta la vista

--- resources/views/admin/grupos/index.blade.php
+++ resources/views/admin/grupos/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-<h2>Gestion de grupos</h2> <br>
+<h2>Gestión de grupos</h2> <br>
 <a href="{{ route('grupos.create') }} " class="btn btn-primary" style="margin-bottom:1em">Nuevo grupo</a>
 
 <!-- Tabla que muestra las jornadas -->
@@ -9,23 +9,21 @@
   <table class="table table-striped table-sm">
     <thead>
       <tr>
-        <th scope="col">#</th>
         <th scope="col">Grado</th>
         <th scope="col">Grupo</th>
         <th scope="col">Jornada</th>
-        <th scope="col">Accion</th>
+        <th scope="col">Acción</th>
       </tr>
     </thead>
     <tbody>
       @foreach($grupos as $grupo)
         <tr>
-          <td>{{ $grupo->id }}</td>
           <td>{{ $grupo->grado->nombre}}</td>
           <td>{{ $grupo->nombre }}</td>
           <td>{{ $grupo->grado->jornada->nombre  }}</td>
           <td>
               <a href="{{ route('grupos.edit', $grupo->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
-              <a href="{{ route('grupos.destroy', $grupo->id) }}" onClick="return confirm('¿Esta seguro que desea eliminar este registro?')" class="btn btn-danger btn-sm pull-left">Eliminar</a></td>
+              <a href="{{ route('grupos.destroy', $grupo->id) }}" onClick="return confirm('¿Está seguro que desea eliminar el grupo {{ $grupo->nombre }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a></td>
           </td>
       @endforeach
         </tr>
