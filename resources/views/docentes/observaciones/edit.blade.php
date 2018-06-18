671c3ad4592cd322fc05223d5b1100b29b88d948
florezsebas <florezsebas@utp.edu.co>
2018-06-18 15:16:11 +0000

    Se ajusta vista

--- resources/views/docentes/observaciones/edit.blade.php
+++ resources/views/docentes/observaciones/edit.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Editar observacion</h2> <br>
+<h2>Editar observación</h2> <br>
 {!! Form::open(['route' => ['observaciones.actualizar', $observacion], 'method' => 'put']) !!}
   <div class="form-row">
     <div class="form-group col-md-3">
-      {!! Form::label('titulo', 'Titulo') !!}
+      {!! Form::label('titulo', 'Título') !!}
       {!! Form::text('titulo', $observacion->titulo, ['class' => 'form-control','placeholder' => 'Titulo', 'required']) !!}
     </div>
     <div class="form-group col-md-3">
@@ -14,12 +14,12 @@
     </div>
   </div>
   <div>
-    {!! Form::label('descripcion', 'Descripcion') !!}
+    {!! Form::label('descripcion', 'Descripción') !!}
   </div>
   <div class="form-group">
-    {!! Form::textarea('descripcion', $observacion->descripcion, ['class' => 'form-control','placeholder' => 'Descripcion', 'required']) !!}
+    {!! Form::textarea('descripcion', $observacion->descripcion, ['class' => 'form-control', 'required']) !!}
   </div>
-  {!! Form::submit('Editar observacion', ['class' => 'btn btn-primary']) !!}
+  {!! Form::submit('Editar observación', ['class' => 'btn btn-primary']) !!}
   <a href="{{ route('observaciones.listar', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
 @endsection
