c8b7ff1b29f4d1b59c2ad63b1890a9e4117ca364
florezsebas <florezsebas@utp.edu.co>
2018-06-18 14:25:40 +0000

    Se ajusta vista

--- resources/views/docentes/actividades/list.blade.php
+++ resources/views/docentes/actividades/list.blade.php
@@ -4,7 +4,7 @@
 <h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2> <br>
 <a href="{{ route('actividades.create', $curso->id) }}" class="btn btn-primary" style="margin-bottom:1em">Nueva actividad</a>
 
-<!-- Tabla que muestra las actividades -->
+<!-- Tabla que lista las actividades -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
     <thead>
@@ -21,7 +21,7 @@
           <td>{{ $actividad->porcentaje }}</td>
           <td>
               <a href="{{ route('actividades.edit', $actividad->id)  }}" class="btn btn-primary btn-sm pull-left">Editar</a>
-              <a href="{{ route('actividades.destroy', $actividad->id) }}" class="btn btn-danger btn-sm pull-left">Eliminar</a>
+              <a href="{{ route('actividades.destroy', $actividad->id) }}" onClick="return confirm('¿Está seguro que desea eliminar la actividad {{ $actividad->nombre }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a>
           </td>
         </tr>
       @endforeach
