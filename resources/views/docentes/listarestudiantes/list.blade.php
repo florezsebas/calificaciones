9fb222ab17d898165b69f16fce4cb65239b1f7da
florezsebas <florezsebas@utp.edu.co>
2018-06-18 13:57:52 +0000

    Se ajusta mensajes para listar los estudiantes de un curso seleccionado

--- resources/views/docentes/listarestudiantes/list.blade.php
+++ resources/views/docentes/listarestudiantes/list.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h4>Estudiantes del curso </h4>
+<h4>Estudiantes del curso {{ $curso->nombre }} grado {{ $curso->grupo->grado->nombre }} grupo {{ $curso->grupo->nombre }} </h4>
+
+<!--Tabla que muestra los estudiantes del curso seleccionado-->
 <div class="table-responsive">
     <table class="table table-striped table-sm">
         <thead>
             <tr>
-                <th scope="col">#</th>
                 <th scope="col">Apellidos</th>
                 <th scope="col">Nombre</th>
-                <th scope="col">Codigo</th>
+                <th scope="col">Código</th>
                 <th scope="col">Correo</th>
             </tr>
         </thead>
         <tbody>
             @foreach($estudiantes as $estudiante)
                 <tr>
-                    <td>{{ $estudiante->user_id }}</td>
                     <td>{{ $estudiante->user->apellidos }}</td>
                     <td>{{ $estudiante->user->nombres }}</td>
                     <td>{{ $estudiante->user->codigo }}</td>
