2a41a25e003941527b9da9d84729d425911c1565
florezsebas <florezsebas@utp.edu.co>
2018-06-18 13:21:10 +0000

    -Se ajsuta mensajes del index

--- resources/views/acudientes/observaciones/index.blade.php
+++ resources/views/acudientes/observaciones/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.acudientes')
 
 @section('content')
+<h2>Observaciones</h2>
 <h2>Estudiantes a cargo</h2>
 <br>
 
@@ -9,18 +10,16 @@
   <table class="table table-striped table-sm">
     <thead>
       <tr>
-        <th scope="col">#</th>
         <th scope="col">Apellidos</th>
         <th scope="col">Nombre</th>
-        <th scope="col">Codigo</th>
+        <th scope="col">Código</th>
         <th scope="col">Correo</th>
-        <th scope="col">Accion</th>
+        <th scope="col">Acción</th>
       </tr>
     </thead>
     <tbody>
       @foreach($estudiantes as $estudiante)
       <tr>
-        <td>{{ $estudiante->user_id }}</td>
         <td>{{ $estudiante->user->apellidos }}</td>
         <td>{{ $estudiante->user->nombres }}</td>
         <td>{{ $estudiante->user->codigo }}</td>
