7a3bae6a6144302353f556e806e69ed8d75c1c10
florezsebas <florezsebas@utp.edu.co>
2018-06-18 16:19:43 +0000

    Se ajusta vista

--- resources/views/admin/periodos/index.blade.php
+++ resources/views/admin/periodos/index.blade.php
@@ -9,23 +9,21 @@
   <table class="table table-striped table-sm">
   <thead>
     <tr>
-      <th scope="col">#</th>
       <th scope="col">Nombre</th>
       <th scope="col">Fecha de inicio</th>
-      <th scope="col">Fecha de finalizacion</th>
+      <th scope="col">Fecha de finalización</th>
       <th scope="col">Accion</th>
     </tr>
   </thead>
   <tbody>
     @foreach($periodos as $periodo)
       <tr>
-        <td>{{ $periodo->id }}</td>
         <td>{{ $periodo->nombre }}</td>
         <td>{{ $periodo->fecha_inicio }}</td>
         <td>{{ $periodo->fecha_fin }}</td>
         <td>
             <a href="{{ route('periodos.edit', $periodo->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
-            <a href="{{ route('periodos.destroy', $periodo->id) }}" onClick="return confirm('¿Esta seguro que desea eliminar este registro?')" class="btn btn-danger btn-sm pull-left">Eliminar</a></td>
+            <a href="{{ route('periodos.destroy', $periodo->id) }}" onClick="return confirm('¿Esta seguro que desea eliminar el periodo {{ $periodo->nombre }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a></td>
         </td>
     @endforeach
       </tr>
