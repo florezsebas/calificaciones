00aa3feed244045612f68447708732a8fb21e579
florezsebas <florezsebas@utp.edu.co>
2018-06-18 14:33:16 +0000

    Se ajusta vista

--- resources/views/docentes/calificaciones/index.blade.php
+++ resources/views/docentes/calificaciones/index.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Calificaciones</h2> <br>
+<h2>Cursos</h2> <br>
 
 <!-- Tabla que muestra los cursos -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
     <thead>
       <tr>
-        <th scope="col">#</th>
         <th scope="col">Jornada</th>
         <th scope="col">Nombre</th>
-        <th scope="col">Docente</th>
-        <th scope="col">Grado:Grupo</th>
-        <th scope="col">Accion</th>
+        <th scope="col">Docente a cargo</th>
+        <th scope="col">Grado : Grupo</th>
+        <th scope="col">Acción</th>
       </tr>
     </thead>
     <tbody>
       @foreach($cursos as $curso)
         <tr>
-          <td>{{ $curso->id }}</td>
           <td>{{ $curso->grupo->grado->jornada->nombre }}</td>
           <td>{{ $curso->nombre }}</td>
           <td>{{ $curso->docente->user->nombres }}</td>
