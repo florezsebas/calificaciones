0c7e8d18eb3e06fad0e182a237722ed6f42e7f35
florezsebas <florezsebas@utp.edu.co>
2018-06-19 03:20:36 +0000

    Se agrega boton atras

--- resources/views/docentes/calificaciones/listarcursos.blade.php
+++ resources/views/docentes/calificaciones/listarcursos.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2> <br>
+<h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
+<br>
+<a href="{{ route('calificaciones.docentes.index') }}" class="btn btn-primary btn-bg pull-left">Atrás</a>
+<br>
+<br>
 <!-- Tabla que muestra las actividades -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
