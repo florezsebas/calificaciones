8799a7be02ab3d773347e33df2ab259036e57352
florezsebas <florezsebas@utp.edu.co>
2018-06-19 02:56:08 +0000

    Se agrega boton cancelar y muestra mensaje sobre el curso

--- resources/views/docentes/actividades/edit.blade.php
+++ resources/views/docentes/actividades/edit.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Editar actividad</h2> <br>
+<h2>Editar actividad para el curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
+<br>
 {!! Form::open(['route' => ['actividades.update', $actividad], 'method' => 'put']) !!}
   <div class="form-row">
     <div class="form-group col-md-3">
@@ -13,6 +14,7 @@
       {!! Form::number('porcentaje', $actividad->porcentaje, ['class' => 'form-control', 'step' => "0.01",'placeholder' => 'Porcentaje','min'=>'0', 'max'=>$porcentaje_disp, 'required']) !!}
     </div>
   </div>
-{!! Form::submit('Editar actividad', ['class' => 'btn btn-primary']) !!}
+{!! Form::submit('Guardar cambios', ['class' => 'btn btn-primary']) !!}
+<a href="{{ route('actividades.list',$curso->id) }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
 @endsection
