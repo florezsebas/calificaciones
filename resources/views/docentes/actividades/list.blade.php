53155fca3c182e3c09ada947eef8af5c0751629f
florezsebas <florezsebas@utp.edu.co>
2018-06-19 02:55:08 +0000

    Se agrega nuevo boton atras

--- resources/views/docentes/actividades/list.blade.php
+++ resources/views/docentes/actividades/list.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2> <br>
+<h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
+<br>
 <a href="{{ route('actividades.create', $curso->id) }}" class="btn btn-primary" style="margin-bottom:1em">Nueva actividad</a>
-
+<a href="{{ route('actividades.index') }}" class="btn btn-primary" style="margin-bottom:1em">Atrás</a>
 <!-- Tabla que lista las actividades -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
