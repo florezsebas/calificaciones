302c7915590c6bb8e8b2320d1d463a2c8bb19266
florezsebas <florezsebas@utp.edu.co>
2018-06-19 03:41:48 +0000

    Se agrega boton atras

--- resources/views/docentes/observaciones/list.blade.php
+++ resources/views/docentes/observaciones/list.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.docentes')
 
 @section('content')
-<h2>Estudiante {{ $estudiante->user->apellidos }} {{ $estudiante->user->nombres }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
+<h2>Observaciones del estudiante {{ $estudiante->user->apellidos }} {{ $estudiante->user->nombres }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
 <br>
-<a href="{{ route('observaciones.crear', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary btn-sm pull-left">Nueva observación</a>
-
+<a href="{{ route('observaciones.crear', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary btn-bg pull-left">Nueva observación</a>
+<a href="{{ route('observaciones.estudiantes', $curso->id) }}" class="btn btn-primary btn-bg pull-left">Atrás</a>
+<br>
+<br>
 <!-- Tabla que muestra las observaciones de un estudiante -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
