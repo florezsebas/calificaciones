2a6e13ca00c3b20e9653c0c8dff4e7192727efae
florezsebas <florezsebas@utp.edu.co>
2018-06-20 16:24:32 +0000

    Se realiza ajuste de jerarquia de acciones

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+  
+  <title>{{ config('app.name', 'Portal Administrador') }}</title>
+  
+  <!-- Libreria JQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
+  <!--- Boostrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  
+  <!-- Hoja de estilos -->
+  {{ Html::style('css/dashboard.css', array(), true) }}
 
-    <title>{{ config('app.name', 'Portal Administrador') }}</title>
-   
-    <!-- Libreria JQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
-    <!--- Boostrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-    <!-- Hoja de estilos -->
-    {{ Html::style('css/dashboard.css', array(), true) }}
-    
-  </head>
+</head>
 
   <body>
+    @include('flash::message')
     <!--Barra de navegacion superior-->
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Acciones administrador</a>
+      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/admin">Acciones administrador</a>
+      
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <form method="POST" action="{{ route('logout') }}">
@@ -32,81 +34,92 @@
           </form>
         </li>
       </ul>
+
     </nav>
 
     <div class="container-fluid" id="app">
       <div class="row">
         <div class="container">
+
           <!-- Barra de navegacion lateral ziquierda -->
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link" href="/admin">
-                  <span data-feather="home"></span>
-                  Panel de administracion
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('admin/periodos') }}">
-                  <span data-feather="calendar"></span>
-                  Gestionar periodos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('admin/jornadas') }}">
-                  <span data-feather="clock"></span>
-                  Gestionar jornadas
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#item-1" data-parent="#accordion1">
-                <span data-feather="users"></span>
-                Gestionar usuarios
-                </a>
-                  <li class="nav-item">
-                    <div id="item-1" class="collapse show">
-                      <ul class="nav flex-column ml-3">
-                        <li class="nav-item">
-                          <a class="nav-link" href="{{ url('admin/usuarios/docentes') }}">Docentes</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="{{ url('admin/usuarios/estudiantes') }}">Estudiantes</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="{{ url('admin/usuarios/acudientes') }}">Acudientes</a>
-                        </li>
-                      </ul>
-                    </div>
+          <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <div class="sidebar-sticky">
+              <ul class="nav flex-column">
+                
+                <li class="nav-item">
+                  <a class="nav-link" href="/admin">
+                    <span data-feather="home"></span>
+                    Panel de administracion
+                  </a>
                 </li>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('admin/grados') }}">
-                  <span data-feather="bar-chart-2"></span>
-                  Gestionar grados
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('admin/grupos') }}">
-                  <span data-feather="layers"></span>
-                  Gestionar grupos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('admin/cursos') }}">
-                  <span data-feather="list"></span>
-                  Gestionar cursos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('cambiar_contraseña') }}">
-                  <span data-feather="lock"></span>
-                  Cambiar contraseña
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="collapse" href="#item-1" data-parent="#accordion1">
+                  <span data-feather="users"></span>
+                  Gestionar usuarios
+                  </a>
+                    <li class="nav-item">
+                      <div id="item-1" class="collapse hide">
+                        <ul class="nav flex-column ml-3">
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ url('admin/usuarios/docentes') }}">Docentes</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ url('admin/usuarios/estudiantes') }}">Estudiantes</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ url('admin/usuarios/acudientes') }}">Acudientes</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('admin/periodos') }}">
+                    <span data-feather="calendar"></span>
+                    Gestionar periodos
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('admin/jornadas') }}">
+                    <span data-feather="clock"></span>
+                    Gestionar jornadas
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('admin/grados') }}">
+                    <span data-feather="bar-chart-2"></span>
+                    Gestionar grados
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('admin/grupos') }}">
+                    <span data-feather="layers"></span>
+                    Gestionar grupos
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('admin/cursos') }}">
+                    <span data-feather="list"></span>
+                    Gestionar cursos
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('cambiar_contraseña') }}">
+                    <span data-feather="lock"></span>
+                    Cambiar contraseña
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+          </nav>
         </div>
         
 
