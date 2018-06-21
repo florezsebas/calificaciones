0a647e10e6f24a6911bf9b28dc916929f42ddb21
florezsebas <florezsebas@utp.edu.co>
2018-06-21 13:27:34 +0000

    Se agrega campo de digitacion

--- resources/views/docentes/observaciones/edit.blade.php
+++ resources/views/docentes/observaciones/edit.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 <h2>Editar observación</h2> <br>
-{!! Form::open(['route' => ['observaciones.actualizar', $observacion], 'method' => 'put']) !!}
+{!! Form::open(['route' => ['observaciones.update', $observacion], 'method' => 'put']) !!}
+
+  <div class="form-row">
+    <div class="form-group col-md-3">
+      {!! Form::label('periodo_id', 'Periodo') !!}
+      {!! Form::select('periodo_id', $periodos, $observacion->periodo_id, ['class' => 'form-control','placeholder' => 'Seleccione periodo', 'required']) !!}
+    </div>
+  </div>
+
   <div class="form-row">
     <div class="form-group col-md-3">
       {!! Form::label('titulo', 'Título') !!}
       {!! Form::text('titulo', $observacion->titulo, ['class' => 'form-control','placeholder' => 'Titulo', 'required']) !!}
     </div>
-    <div class="form-group col-md-3">
-      {!! Form::label('periodo', 'Periodo') !!}
-      {!! Form::select('periodo', $periodos, $observacion->periodo_id, ['class' => 'form-control','placeholder' => 'Seleccione periodo', 'required']) !!}
+    <div class="form-group">
+      {!! Form::label('fecha_digitacion', 'Fecha') !!}
+      {!! Form::date('fecha_digitacion', $observacion->fecha_digitacion, ['class' => 'form-control', 'placeholder' => '', 'required']) !!}
     </div>
   </div>
   <div>
