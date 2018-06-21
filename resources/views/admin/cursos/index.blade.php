0a8b8c51aa1d791b626a8719abc6c92210aa5382
florezsebas <florezsebas@utp.edu.co>
2018-06-21 21:47:53 +0000

    Se muestran los periodos

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-<h2>Gestión de cursos</h2> <br>
-<a href="{{ route('cursos.create') }}" class="btn btn-primary" style="margin-bottom:1em">Nuevo curso</a>
+<h2>Periodos</h2> <br>
 
 <!-- Tabla que muestra las jornadas -->
 <div class="table-responsive">
@@ -10,22 +9,19 @@
     <thead>
       <tr>
         <th scope="col">Nombre</th>
-        <th scope="col">Profesor</th>
-        <th scope="col">Jornada</th>
-        <th scope="col">Grado : Grupo</th>
+        <th scope="col">Fecha inicio</th>
+        <th scope="col">Fecha fin</th>
         <th scope="col">Acción</th>
       </tr>
     </thead>
     <tbody>
-      @foreach($cursos as $curso)
+      @foreach($periodos as $periodo)
         <tr>
-          <td>{{ $curso->nombre }}</td>
-          <td>{{ $curso->docente->user->full_name }}</td>
-          <td>{{ $curso->grupo->grado->jornada->nombre }}</td>
-          <td>{{ $curso->grupo->grado->nombre ." ". $curso->grupo->nombre}}</td>
+          <td>{{ $periodo->nombre }}</td>
+          <td>{{ $periodo->fecha_inicio }}</td>
+          <td>{{ $periodo->fecha_fin }}</td>
           <td>
-              <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
-              <a href="{{ route('cursos.destroy', $curso->id) }}" onClick="return confirm('¿Esta seguro que desea eliminar el curso {{ $curso->nombre }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a>
+              <a href="{{ route('cursos.list', $periodo->id) }}" class="btn btn-primary btn-sm pull-left">Ver cursos</a>
           </td>
         </tr>
       @endforeach
