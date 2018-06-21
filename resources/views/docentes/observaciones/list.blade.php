06c5de05ed483801c8ab94c6200ba96071e34801
florezsebas <florezsebas@utp.edu.co>
2018-06-21 13:27:52 +0000

    Se ajusta vista

--- resources/views/docentes/observaciones/list.blade.php
+++ resources/views/docentes/observaciones/list.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <h2>Observaciones del estudiante {{ $estudiante->user->apellidos }} {{ $estudiante->user->nombres }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
 <br>
-<a href="{{ route('observaciones.crear', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary btn-bg pull-left">Nueva observación</a>
+<h4>Curso: {{ $curso->nombre }}</h4>
+<a href="{{ route('observaciones.create', [$curso->id, $estudiante->user_id]) }}" class="btn btn-primary btn-bg pull-left">Nueva observación</a>
 <a href="{{ route('observaciones.estudiantes', $curso->id) }}" class="btn btn-primary btn-bg pull-left">Atrás</a>
 <br>
 <br>
@@ -13,17 +14,19 @@
     <thead>
       <tr>
         <th scope="col">Título</th>
-        <th scope="col">Acción</th>
+        <th scope="col">Fecha digitación</th>
+        <th scope="col">Acciones</th>
       </tr>
     </thead>
     <tbody>
       @foreach($observaciones as $observacion)
         <tr>
           <td>{{ $observacion->titulo }}</td>
+          <td>{{ $observacion->fecha_digitacion}}</td>
           <td>
               <a href="{{ route('observaciones.show', [$curso->id, $estudiante->user_id, $observacion->id]) }}" class="btn btn-primary btn-sm pull-left">Ver</a>
-              <a href="{{ route('observaciones.editar', $observacion->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
-              <a href="{{ route('observaciones.destruir', $observacion->id) }}" onClick="return confirm('¿Está seguro que desea eliminar la observacion {{ $observacion->titulo }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a>
+              <a href="{{ route('observaciones.edit', $observacion->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
+              <a href="{{ route('observaciones.destroy', $observacion->id) }}" onClick="return confirm('¿Está seguro que desea eliminar la observacion {{ $observacion->titulo }}?')" class="btn btn-danger btn-sm pull-left">Eliminar</a>
           </td>
         </tr>
       @endforeach
