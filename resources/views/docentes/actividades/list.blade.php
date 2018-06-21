c32a8d9911e146b9d0df85242038d6d653d33ae2
florezsebas <florezsebas@utp.edu.co>
2018-06-21 23:22:49 +0000

    Se ajusta la vista

--- resources/views/docentes/actividades/list.blade.php
+++ resources/views/docentes/actividades/list.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <h2>Actividades del curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
 <br>
-<a href="{{ route('actividades.create', $curso->id) }}" class="btn btn-primary" style="margin-bottom:1em">Nueva actividad</a>
-<a href="{{ route('actividades.index') }}" class="btn btn-primary" style="margin-bottom:1em">Atrás</a>
+<a href="{{ route('actividades.create',[$periodo_id, $curso->id]) }}" class="btn btn-primary" style="margin-bottom:1em">Nueva actividad</a>
+<a href="{{ route('actividades.cursos', $periodo_id) }}" class="btn btn-primary" style="margin-bottom:1em">Atrás</a>
 <!-- Tabla que lista las actividades -->
 <div class="table-responsive">
   <table class="table table-striped table-sm">
