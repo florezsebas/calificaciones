befd1ad7d273068959a2506c97341ad790caf9cf
florezsebas <florezsebas@utp.edu.co>
2018-06-21 23:14:05 +0000

    Se ajusta vista

--- resources/views/docentes/actividades/create.blade.php
+++ resources/views/docentes/actividades/create.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 <h2>Nueva actividad para el curso {{ $curso->nombre  }} del grado {{$curso->grupo->grado->nombre }} {{$curso->grupo->nombre}} Jornada {{$curso->grupo->grado->jornada->nombre}} </h2>
 <br>
-{!! Form::open(['route' => ['actividades.store', $curso], 'method' => 'post']) !!}
-  <div class="form-row">
-    <div class="form-group col-md-3">
-        {!! Form::label('periodo_id', 'Periodos') !!}
-        {!! Form::select('periodo_id', $periodos,null ,['class' => 'form-control','placeholder' => 'Seleccione periodo', 'required' ]) !!}
-    </div>
-  </div>
+{!! Form::open(['route' => ['actividades.store', $periodo_id, $curso], 'method' => 'post']) !!}
   <div class="form-row">
     <div class="form-group col-md-3">
       {!! Form::label('nombre', 'Nombre') !!}
@@ -17,10 +11,10 @@
     </div>
     <div class="form-group col-md-3">
       {!! Form::label('porcentaje', 'Porcentaje') !!}
-      {!! Form::number('porcentaje', null, ['class' => 'form-control', 'step' => "0.01",'placeholder' => 'Porcentaje', 'min'=>'0', 'max'=>$porcentaje_disp, 'required' ]) !!}
+      {!! Form::number('porcentaje', null, ['class' => 'form-control', 'step' => "0.01",'placeholder' => 'Porcentaje', 'min'=>'0.01', 'max'=>$porcentaje_disp, 'required' ]) !!}
     </div>
   </div>
 {!! Form::submit('Agregar actividad', ['class' => 'btn btn-primary']) !!}
-<a href="{{ route('actividades.list',$curso->id) }}" class="btn btn-primary">Cancelar</a>
+<a href="{{ route('actividades.list',[$periodo_id,$curso->id]) }}" class="btn btn-primary">Cancelar</a>
 {!! Form::close() !!}
 @endsection
