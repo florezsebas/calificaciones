42d314c99fa3b52746a86a9fb1930c7878fbd479
florezsebas <florezsebas@utp.edu.co>
2018-06-22 16:13:19 +0000

    Se agrega campo anyo

--- database/migrations/2018_05_16_123451_create_periodos_table.php
+++ database/migrations/2018_05_16_123451_create_periodos_table.php
@@ -18,6 +18,7 @@ class CreatePeriodosTable extends Migration
             $table->string('nombre');
             $table->date('fecha_inicio');
             $table->date('fecha_fin');
+            $table->unsignedInteger('anyo');
             $table->timestamps();
         });
     }
