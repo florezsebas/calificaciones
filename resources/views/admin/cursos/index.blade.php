65962e3d3fe06f9d11ac0cc35c585549b5104ab0
florezsebas <florezsebas@utp.edu.co>
2018-06-22 16:41:18 +0000

    Se agrega campo anyo

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
-<h2>Periodos</h2> <br>
+<h2>Gestión de cursos</h2>
+<br>
+<h4>Periodos</h4>
 
 <!-- Tabla que muestra las jornadas -->
 <div class="table-responsive">
@@ -11,6 +13,7 @@
         <th scope="col">Nombre</th>
         <th scope="col">Fecha inicio</th>
         <th scope="col">Fecha fin</th>
+        <th scope="col">Año lectivo</th>
         <th scope="col">Acción</th>
       </tr>
     </thead>
@@ -20,6 +23,7 @@
           <td>{{ $periodo->nombre }}</td>
           <td>{{ $periodo->fecha_inicio }}</td>
           <td>{{ $periodo->fecha_fin }}</td>
+          <td>{{ $periodo->anyo }}</td>
           <td>
               <a href="{{ route('cursos.list', $periodo->id) }}" class="btn btn-primary btn-sm pull-left">Ver cursos</a>
           </td>
--- resources/views/admin/periodos/index.blade.php
+++ resources/views/admin/periodos/index.blade.php
@@ -12,6 +12,7 @@
       <th scope="col">Nombre</th>
       <th scope="col">Fecha de inicio</th>
       <th scope="col">Fecha de finalización</th>
+      <th scope="col">Año lectivo</th>
       <th scope="col">Accion</th>
     </tr>
   </thead>
@@ -21,6 +22,7 @@
         <td>{{ $periodo->nombre }}</td>
         <td>{{ $periodo->fecha_inicio }}</td>
         <td>{{ $periodo->fecha_fin }}</td>
+        <td>{{ $periodo->anyo }}</td>
         <td>
             <a href="{{ route('periodos.edit', $periodo->id) }}" class="btn btn-primary btn-sm pull-left">Editar</a>
             <a href="{{ route('periodos.destroy', $periodo->id) }}" onClick="return confirm('¿Está seguro que desea eliminar el periodo {{ $periodo->nombre }}?, esto podría implicar la eliminación en cascada de otros elementos que dependan de este registro (Ejemplo: La eliminación de todos los cursos asociados al periodo)')" class="btn btn-danger btn-sm pull-left">Eliminar</a></td>
