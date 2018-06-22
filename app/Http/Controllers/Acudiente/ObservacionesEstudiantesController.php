157daca5e8453fed462e2a108e2f91c504959a21
florezsebas <florezsebas@utp.edu.co>
2018-06-22 06:36:04 +0000

    Se ajusta contrador con periodo

--- app/Http/Controllers/Acudiente/ObservacionesEstudiantesController.php
+++ app/Http/Controllers/Acudiente/ObservacionesEstudiantesController.php
@@ -9,22 +9,38 @@ use App\Acudiente;
 use App\Estudiante;
 use App\Curso;
 use App\Observacion;
+use App\Periodo;
 
 class ObservacionesEstudiantesController extends Controller
 {
     public function index(Request $request)
     {
         $usuario = Auth::user();
-        $acudiente = $usuario->acudiente;
-        $estudiantes = $acudiente->estudiantes;
-        return view('acudientes.observaciones.index')->with('estudiantes',$estudiantes);
+        $estudiantes_a_cargo = $usuario->acudiente->estudiantes;
+        $estudiant = array();
+        foreach($estudiantes_a_cargo as $estudiante)
+            $estudiant[$estudiante->user_id] = $estudiante->user->nombres;
+        $estudiantes = collect($estudiant);
+        $use_request = 1;
+        $periodos = Periodo::all()->pluck('nombre', 'id');
+        return view('acudientes.observaciones.index')->with('use_request',$use_request)
+                                                    ->with('estudiantes',$estudiantes)
+                                                    ->with('periodos',$periodos);
     }
-    
-    public function listingCourses(Request $request, $user_id)
+
+    public function listingCourses(Request $request,$periodo_id,$user_id,$use_request)
     {
-        $estudiante = Estudiante::find($user_id);
+        $estudiante_id = $request->user_id;
+        $periodo_id = $request->periodo_id;
+        if(!$use_request)
+        {
+            $estudiante_id = $user_id;
+            $periodo_id = $periodo_id;
+        }
+        
+        $estudiante = Estudiante::find($estudiante_id);
         $grupo = $estudiante->grupo;
-        $cursos = $grupo->cursos;
+        $cursos = Curso::where('periodo_id',$periodo_id)->where('grupo_id', $grupo->id)->get();
         return view('acudientes.observaciones.listarcursos')->with('cursos',$cursos)
                                         ->with('estudiante',$estudiante);
     }
@@ -34,7 +50,9 @@ class ObservacionesEstudiantesController extends Controller
         $curso = Curso::find($curso_id);
         $estudiante = Estudiante::find($user_id);
         $observaciones = Observacion::where('curso_id',$curso_id)->where('estudiante_id',$user_id)->get();
-        return view('acudientes.observaciones.listarobservaciones')->with('estudiante',$estudiante)
+        $dont_use_request = 0;
+        return view('acudientes.observaciones.listarobservaciones')->with('dont_use_request',$dont_use_request)
+                                        ->with('estudiante',$estudiante)
                                         ->with('curso',$curso)
                                         ->with('observaciones',$observaciones);
     }
