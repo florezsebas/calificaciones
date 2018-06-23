3dc5cf83fea37f3729828aad17712938eb268b8e
florezsebas <florezsebas@utp.edu.co>
2018-06-23 02:50:58 +0000

    Se calcula el promedio del año

--- app/Http/Controllers/Acudiente/CalificacionesEstudiantesController.php
+++ app/Http/Controllers/Acudiente/CalificacionesEstudiantesController.php
@@ -16,14 +16,7 @@ class CalificacionesEstudiantesController extends Controller
     {
         $usuario = Auth::user();
         $acudiente = $usuario->acudiente;
-        //$estudiantes_a_cargo = $acudiente->estudiantes;
         $estudiantes = $acudiente->estudiantes;
-        //dd($estudiantes->first()->user);
-        //$estudiant = array();
-        //$cursos = $grupo->cursos;
-        // foreach($estudiantes_a_cargo as $estudiante)
-        //     $estudiant[$estudiante->user_id] = $estudiante->user->nombres;
-        // $estudiantes = collect($estudiant);
         $periodos = Periodo::all()->pluck('nombre','id');
         return view('acudientes.calificaciones.index')->with('periodos',$periodos)
                                                       ->with('estudiantes',$estudiantes);
@@ -32,15 +25,36 @@ class CalificacionesEstudiantesController extends Controller
     public function listingGrades(Request $request, $user_id)
     {
         $estudiante = Estudiante::find($user_id);
-        //dd($user_id);
         $periodo = Periodo::find($request->periodo_id);
-        //dd($request->all());
-        //dd($estudiante);
         $grupo = $estudiante->grupo;
         $cursos = $grupo->cursos;
         $periodos = Periodo::all()->pluck('nombre','id');
+        
+        $promedios_cursos = array();
+        foreach($cursos as $curso) {
+            $promedio = 0.0;
+            foreach($curso->actividades as $actividad) {
+              $nota = $actividad->obtenerCalificacion($estudiante->user_id); 
+              $porcentaje = $actividad->porcentaje;
+              $promedio += $nota * ($porcentaje/100);
+              $promedio = number_format((float)$promedio, 1, '.', '');
+            }
+            
+            if(array_key_exists($curso->nombre,$promedios_cursos))
+                $promedios_cursos[$curso->nombre] += $promedio;
+            else
+                $promedios_cursos[$curso->nombre] = $promedio;
+        }
+        
+        $promedio_final_cursos = array();
+        foreach($promedios_cursos as $curso_nombre => $promedio)
+            if(count($periodos))
+                $promedio_final_cursos[$curso_nombre] =  number_format((float)($promedio/count($periodos)),1,'.','' );
+
+        
         $cursos = Curso::where('periodo_id',$request->periodo_id)->where('grupo_id', $grupo->id)->get();
-        return view('acudientes.calificaciones.listar')->with('user_id',$user_id)
+        return view('acudientes.calificaciones.listar')->with('promedio_final_cursos',$promedio_final_cursos)
+                                                       ->with('user_id',$user_id)
                                                        ->with('periodos',$periodos)
                                                        ->with('cursos',$cursos)
                                                        ->with('estudiante',$estudiante)
