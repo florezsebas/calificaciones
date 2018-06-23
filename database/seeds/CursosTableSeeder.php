052d67ea4dddbae2304f3bc57a880479b8e8a572
florezsebas <florezsebas@utp.edu.co>
2018-06-23 02:29:54 +0000

    Se modifica la creacion dinamica de cursos

--- database/seeds/CursosTableSeeder.php
+++ database/seeds/CursosTableSeeder.php
@@ -11,32 +11,34 @@ class CursosTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('cursos')->insert([
-            'nombre' => 'Matematicas',
-            'docente_id' => DB::table('users')->where('nombres', 'Carlos Alberto')->value('id'),
-            'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
-            'periodo_id' => DB::table('periodos')->where('id', 1)->value('id'),
-        ]);
-        
-        DB::table('cursos')->insert([
-            'nombre' => 'Matematicas',
-            'docente_id' => DB::table('users')->where('nombres', 'Carlos Alberto')->value('id'),
-            'grupo_id' => DB::table('grupos')->where('id', 2)->value('id'),
-            'periodo_id' => DB::table('periodos')->where('id', 1)->value('id'),
-        ]);
-        
-        DB::table('cursos')->insert([
-            'nombre' => 'Educacion Fisica',
-            'docente_id' => DB::table('users')->where('nombres', 'Mario')->value('id'),
-            'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
-            'periodo_id' => DB::table('periodos')->where('id', 1)->value('id'),
-        ]);
-        
-        DB::table('cursos')->insert([
-            'nombre' => 'Matematicas',
-            'docente_id' => DB::table('users')->where('nombres', 'Mario')->value('id'),
-            'grupo_id' => DB::table('grupos')->where('id', 2)->value('id'),
-            'periodo_id' => DB::table('periodos')->where('id', 1)->value('id'),
-        ]);
+        for($i=1; $i<5; $i++){
+            DB::table('cursos')->insert([
+                'nombre' => 'Matematicas',
+                'docente_id' => DB::table('users')->where('nombres', 'Carlos Alberto')->value('id'),
+                'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
+                'periodo_id' => $i,
+            ]);    
+                
+            DB::table('cursos')->insert([
+                'nombre' => 'Filosofia',
+                'docente_id' => DB::table('users')->where('nombres', 'Carlos Alberto')->value('id'),
+                'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
+                'periodo_id' => $i,
+            ]);
+            
+            DB::table('cursos')->insert([
+                'nombre' => 'Educacion Fisica',
+                'docente_id' => DB::table('users')->where('nombres', 'Mario')->value('id'),
+                'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
+                'periodo_id' => $i,
+            ]);
+            
+            DB::table('cursos')->insert([
+                'nombre' => 'Etica',
+                'docente_id' => DB::table('users')->where('nombres', 'Mario')->value('id'),
+                'grupo_id' => DB::table('grupos')->where('id', 1)->value('id'),
+                'periodo_id' => $i,
+            ]);
+        }
     }
 }
