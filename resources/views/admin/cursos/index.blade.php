35f94f1dc7fbfc110af1f285898295c969cd474a
florezsebas <florezsebas@utp.edu.co>
2018-06-23 17:59:13 +0000

    Se ajusta vista

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 <h2>Gestión de cursos</h2>
 <br>
+<p style="color:red; font-size:15px;"> 
+  Recuerde tener en cuenta que el nombre de un mismo curso que cree en cada periodo, 
+  debe ser exactamente el mismo, ya que si no es así, el sistema tendrá en cuenta
+  ese curso como otro diferente en el año. <br> <br>
+  
+  Ejemplo: 'Matematicas' del periodo 1, 'matematicas' del periodo 2 <br>
+  En este caso las matematicas de ambos periodos no son el mismo ya que el nombre
+  de cada curso comienza con la letra 'm' diferente. 
+  
+</p>
 <h4>Periodos</h4>
 
 <!-- Tabla que muestra las jornadas -->
